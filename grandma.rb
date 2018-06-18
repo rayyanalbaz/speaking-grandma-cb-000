682bcccbc9f45dceec31afdaa448d590bb9e6114
Rayyan A. Albaz <ra5analbaz@hotmail.com>
2018-06-18 02:11:05 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,11 +3,8 @@
     uc = "#{phrase}".upcase
     if "#{phrase}" != uc
       puts "HUH?! SPEAK UP, SONNY!"
-    elsif "#{phrase}" == uc && "#{phrase}" != "I LOVE YOU GRANDMA!"
-      puts "NO, NOT SINCE 1938!"
-    elsif "#{phrase}" == "I LOVE YOU GRANDMA!"
-      puts "I LOVE YOU TOO PUMPKIN!"
     end
+
 
 
 
